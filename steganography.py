b9e393d931c0ee1dd1ccf9e5ef87a2ea10b736fb
m-fedosov <90381679+m-fedosov@users.noreply.github.com>
2021-11-28 18:10:14 +0300

    correct psnr

--- steganography.py
+++ steganography.py
@@ -3,7 +3,8 @@ from math import log10
 choice = input(':: Стеганография (Михаил Федосов):: '
                '\n1 - зашифровать информацию в изображении '
                '\n2 - расшифровать информацию в изображении \n')
-path = input('Укажите путь к изображению-контейнеру в формате (.png): \n')  # C:\Users\mmmfe\Pictures\Saved Pictures\draw.png
+path = input('Укажите путь к изображению-контейнеру в формате (.png): '
+             '\n')  # C:\Users\mmmfe\Pictures\Saved Pictures\draw.png
 img = Image.open(path)  # C:\Users\mmmfe\Pictures\Saved Pictures\stego_img.png
 
 
@@ -13,19 +14,18 @@ def your_message(message_type):
         bit_bits += '100110101001001010001100110010101100100'  # обозначение конца строки
         bit_bits = '01' + bit_bits  # обозначение битовой строки для автоматической дешифровки
         if len(bit_bits) > img.size[0] * img.size[1] * 3:
-            print('Размер картинки слишком маленький для введённой последовательности бит \n'+
-                  'Количество бит скрытой инвормации: '+ str(len(bin_bits)) +
+            print('Размер картинки слишком маленький для введённой последовательности бит \n'
+                  'Количество бит скрытой инвормации: ' + str(len(bin_bits)) +
                   '\nв картинке свободных бит для скрытия: ' + str(img.size[0] * img.size[1] * 3))
             exit()
         return bit_bits
     elif message_type == 2:  # txt xtr
         choose_input = int(input('Выберите: \nВвести текст врчную - 1 \nОткрыть файл (название.txt) - 2\n'))
         if choose_input == 1:
-            text = input('Введите текст для стеганографии (без символов русского алфавита): ')
+            text = input('Введите текст для стеганографии (без символов русского алфавита): ')  # Slave
         elif choose_input == 2:
             text_file = input('Введите путь к файлу .txt: \n')  # D:\Загрузки\lord.txt
             text = open(text_file, 'r').read()
-            print(text)
         else:
             print('Некорректный ввод в строке выше')
             exit()
@@ -37,15 +37,15 @@ def your_message(message_type):
                 sign = '0' * (8 - len(sign)) + sign
             bit_text += sign
         if len(bit_text) > img.size[0] * img.size[1] * 3:
-            print('Размер картинки слишком маленький для введённой последовательности бит \n'+
+            print('Размер картинки слишком маленький для введённой последовательности бит \n'
                   'Количество бит скрытой инвормации: ' + str(len(bit_text)) +
                   '\nв картинке свободных бит для скрытия: ' + str(img.size[0] * img.size[1] * 3))
             exit()
-        print(bit_text[2:])
         return bit_text
     elif message_type == 3:  # img
         path_stego = input(
-            'Укажите путь к изображению в формате (.png), которое хотите скрыть: \n')  # C:\Users\mmmfe\Pictures\Saved Pictures\draw.png
+            'Укажите путь к изображению в формате (.png), которое хотите скрыть:'
+            ' \n')  # C:\Users\mmmfe\Pictures\Saved Pictures\draw.png
         img_stego = Image.open(path_stego)
         pixel_array = img_stego.load()
         stego_pixels = '11'  # обозначение картинки для дешифровки
@@ -63,21 +63,21 @@ def your_message(message_type):
                   '\n- выберите картинку для зашифрования меньшего размера'
                   '\n- предложите картинку большего разрешения на первом шаге')
             exit()
-        return(stego_pixels)
+        return stego_pixels
     else:
         print('Некорректный ввод')
     exit()
 
 
-def save_picture(img):
+def save_picture(image):
     image_name = input('Введите название файла для сохранения (название.png): \n')
     choose_path = input('- Напишите адрес директории, где хотите сохранить стего-картинку \n'
                         '- Введите 0 и картинка сохранится рядом с программой: ')
     if choose_path == '0':
-        img.save(image_name)
+        image.save(image_name)
     else:
         path_picture = (choose_path + '\\' + image_name)
-        img.save(path_picture)
+        image.save(path_picture)
         print('Картинка сохранена по пути:\n' + path_picture)
 
 
@@ -119,23 +119,24 @@ def encode():
                 change_pixel = list(change_px_array[i, j])
                 for k in range(3):
                     if original_pixel[k] != change_pixel[k]:
-                        cnt_dfrnt += 1
-        mse = (1 / (m * n)) * cnt_dfrnt ** 2  # среднеквадратичная ошибка
-        psnr = 10 * log10(255**2/mse)  # пиковое отношение сигнал-шум
-        print(psnr)
+                        cnt_dfrnt += 1  # не возвожу в квадрат, т.к. 1^2 = 1
+
+        mse = cnt_dfrnt/(m * n * 3)  # среднеквадратичная ошибка
+        psnr = 10 * log10(255**2/(mse**2))  # пиковое отношение сигнал-шум
+        print('значение PSNR:', psnr, 'dB')
 
 
 def decode_by_type(bits):
-    type = bits[:2]
-    if type == '01':  # битовая строка
+    stego_type = bits[:2]
+    if stego_type == '01':  # битовая строка
         print(bits[2:])
-    elif type == '10':  # текст
+    elif stego_type == '10':  # текст
         stego_text = bits[2:]
         text = ''
         for i in range(len(stego_text) // 8):
             text += chr(int('0b' + stego_text[8 * i:8 * i + 8], 2))
         print(text)
-    elif type == '11':  # картинка
+    elif stego_type == '11':  # картинка
         stego_bits = bits[26:]
         x = int(bits[2:14], 2)
         y = int(bits[14:26], 2)
@@ -177,4 +178,4 @@ elif choice == '2':
     decode()
 else:
     print('Неизвестная комманда')
-exit = input('Нажмите Enter для выхода ')
+exit = input(':: Нажмите Enter для выхода ::')
